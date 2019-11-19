bbe33fa38079916cc73d5e7091553a0be84ad3f4
Sean Lee <seanleecoder@gmail.com>
2019-11-19 13:26:32 +0800

    * 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,18 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Container from "../components/container"
+import styles from "./404.module.css"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <div className={styles.errorImageOverlay}>
+        <div className={styles.errorImageContainer}></div>
+        <h2 className={styles.errorImageText}>A Dog Ate this Page</h2>
+      </div>
+    </Container>
   </Layout>
 )
 
